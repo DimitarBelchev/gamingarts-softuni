5842b295a58e8b35077c5795783dcc2578aa6395
DimitarBelchev <52967149+DimitarBelchev@users.noreply.github.com>
2020-08-12 22:45:09 +0300

    init

---
+++ client/src/core/Home.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+import Posts from "../post/Posts";
+
+const Home = () => (
+  <>
+    <div className="container">
+      <Posts />
+    </div>
+    <footer
+      className="page-footer font-small"
+      style={{ background: "#3E4551" }}
+    >
+      <div className="container">
+        <p
+          className="text-center"
+          style={{
+            color: "#fff",
+            fontSize: "large",
+            margin: "0",
+            padding: "20px",
+          }}
+        >
+          <i
+            className="fas fa-registered"
+            style={{ color: "white", fontSize: "24px" }}
+          ></i>{" "}
+          Made by Dimitar Belchev @2020
+        </p>
+      </div>
+    </footer>
+  </>
+);
+
+export default Home;
